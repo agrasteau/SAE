1efd7d52356f91a201e1b29b77dd42a224d24a3d
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2024-01-08 19:50:09 +0100

    template des broadcast

--- broadcastprod.php
+++ broadcastprod.php
@@ -1,5 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>L'étal en ligne</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style_general.css">
+    <link rel="stylesheet" type="text/css" href="css/popup.css">
+</head>
+<body>
+    <?php
+        if(!isset($_SESSION)){
+            session_start();
+        }
+    ?>
+    <div class="container">
+        <div class="leftColumn">
+			<img class="logo" href="index.php" src="img/logo.png">
+        </div>
+        <div class="rightColumn">
+            <div class="contenuPage">
 
-echo 'test'
+               <!-- some code -->
 
-?>
+            </div>
+            <div class="basDePage">
+                <form method="post">
+						<input type="submit" value="Signaler un dysfonctionnement" class="lienPopup">
+                        <input type="hidden" name="popup" value="contact_admin">
+				</form>
+                <form method="post">
+						<input type="submit" value="CGU" class="lienPopup">
+                        <input type="hidden" name="popup" value="cgu">
+				</form>
+            </div>
+        </div>
+    </div>
+    <?php require "popups/gestion_popups.php";?>
+</body>
